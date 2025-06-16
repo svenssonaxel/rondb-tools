c85a5255a1044a7d561df5ec9c888a7c1d20e8a4
Axel Svensson <axel@hopsworks.ai>
2025-06-16 11:17:09 +0200

    [RONDB-912] shell.nix: Use allowUnfreePredicate

--- shell.nix
+++ shell.nix
@@ -3,7 +3,9 @@ let
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.05.tar.gz";
     sha256 = "1915r28xc4znrh2vf4rrjnxldw2imysz819gzhk9qlrkqanmfsxd";
   };
-  pkgs = import nixpkgs { config.allowUnfree = true; };
+  pkgs = import nixpkgs {
+    config.allowUnfreePredicate = pkg: pkgs.lib.getName pkg == "terraform";
+  };
 in
 pkgs.mkShell {
   buildInputs = with pkgs; [
